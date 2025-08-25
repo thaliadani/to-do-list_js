d0ac060e61ff20f88fb827eb090c6c4b03cce9e6
Thalia Danielle <thaliadani2@gmail.com>
2025-08-25 09:53:26 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// O botao para adicionar a tarefa digitada no input
+let botaoAdicionar = document.getElementById('adicionar-tarefa');
+// Div com id tarefas para adicionar a tarefa na lista
+let listaTarefas = document.getElementById('tarefas');
+
+//Evento de clicar no botao para criar uma tarefa
+botaoAdicionar.addEventListener("click", () => {
+    // pega o valor do input
+    let inputTarefa = document.getElementById('tarefa').value;
+    // verifica se o input está vazio e adiciona uma li com o valor 
+    if(inputTarefa !== "" || inputTarefa!== listaTarefas){
+        let novaTarefa = document.createElement("li")
+        novaTarefa.innerText = inputTarefa;
+        listaTarefas.appendChild(novaTarefa)
+    }
+})
